451049ceadd76c6bde4dcfd5a5edef2f0e484ff7
vay-tk <vaytk001@gmail.com>
2025-06-14 22:43:21 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -2,7 +2,7 @@ import React, { useContext, useEffect, useState } from 'react'
 import assets from '../assets/assets'
 import { useNavigate } from 'react-router-dom'
 import { AuthContext } from '../../context/AuthContext'
-import { ChatContext } from '../../context/chatContext'
+import { ChatContext } from '../../context/ChatContext'
 
 const Sidebar = () => {
 
