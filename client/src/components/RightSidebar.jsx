d3a6525f36efc0e5fac49127d108e12ee976093d
vay-tk <vaytk001@gmail.com>
2025-06-14 22:42:51 +0530

    Update RightSidebar.jsx

--- client/src/components/RightSidebar.jsx
+++ client/src/components/RightSidebar.jsx
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from 'react'
 import assets, { imagesDummyData } from '../assets/assets'
-import { ChatContext } from '../../context/chatContext'
+import { ChatContext } from '../../context/ChatContext'
 import { AuthContext } from '../../context/AuthContext'
 
 const RightSidebar = () => {
