6aec50502d0ce865d94f3bac83c821a4bcf7119a
Vaibhav Maurya <142512533+vaibhavr21@users.noreply.github.com>
2025-06-14 22:18:12 +0530

    Delete client directory

--- client/src/lib/utils.js
+++
@@ -1,9 +0,0 @@
-// format msg time
-
-export function formatMessageTime(date){
-    return new Date(date).toLocaleTimeString("en-US", {
-        hour: '2-digit',
-        minute: '2-digit',
-        hour12: false
-    });
-}
