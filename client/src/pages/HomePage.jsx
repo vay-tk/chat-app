f1be64349cb6f18a1d2aa670dc34212f4177274f
vay-tk <vaytk001@gmail.com>
2025-06-14 22:43:45 +0530

    Update HomePage.jsx

--- client/src/pages/HomePage.jsx
+++ client/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import Sidebar from '../components/Sidebar'
 import ChatContainer from '../components/ChatContainer'
 import RightSidebar from '../components/RightSidebar'
 import { useContext } from 'react'
-import { ChatContext } from '../../context/chatContext'
+import { ChatContext } from '../../context/ChatContext'
 
 const HomePage = () => {
   const { selectedUser} = useContext(ChatContext)
