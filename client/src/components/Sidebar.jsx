bb9c525e4ddf5197a2d06d785473d7793fc3a128
vay-tk <vaytk001@gmail.com>
2025-06-15 10:16:35 +0530

    Update Sidebar.jsx

--- client/src/components/Sidebar.jsx
+++ client/src/components/Sidebar.jsx
@@ -10,6 +10,7 @@ const Sidebar = () => {
     const {logout, onlineUsers} = useContext(AuthContext)
 
     const [input, setInput] = useState(false)
+     const [menuOpen, setMenuOpen] = useState(false);
 
     const filteredUsers = input ? users.filter((user)=>user.fullName.toLowerCase().includes(input.toLowerCase())) : users;
 
@@ -24,12 +25,25 @@ const Sidebar = () => {
             <div className='flex justify-between  items-center'>
                 <img src={assets.logo} alt="logo" className='max-w-40' />
                 <div className='relative py-2 group'>
-                    <img src={assets.menu_icon} alt="Menu"  className='max-h-5 cursor-pointer'/>
-                    <div className='absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100 hidden group-hover:block'>
-                        <p onClick={()=>navigate('/profile')} className='cursor-pointer text-sm'>Edit Profile</p>
-                        <hr className='my-2 border-t border-gray-500' />
-                        <p onClick={()=> logout()} className='cursor-pointer text-sm'>Logout</p>
-                    </div>
+                    <img onClick={() => setMenuOpen(!menuOpen)} // toggle dropdown on click
+                        src={assets.menu_icon} alt="Menu"  
+                        className='max-h-5 cursor-pointer'
+                        />
+                    {menuOpen && (
+              <div className="absolute top-full right-0 z-20 w-32 p-5 rounded-md bg-[#282142] border border-gray-600 text-gray-100">
+                <p
+                  onClick={() => {
+                    navigate("/profile");
+                    setMenuOpen(false); // close menu after navigation
+                  }}
+                  className="cursor-pointer text-sm"
+                >
+                  Edit Profile
+                </p>
+                <hr className="my-2 border-t border-gray-500" />
+                <p className="cursor-pointer text-sm">Logout</p>
+              </div>
+            )}
                 </div>
             </div>
 
